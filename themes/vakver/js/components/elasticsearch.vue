6c56abecac017c4801aca31fd6716a6f6b3aefe8
stephan-v <stephan-v@hotmail.com>
2016-06-20 11:53:18 +0200

    Added a regex rewrite filter to remove .wemagine from the image url

--- themes/vakver/js/components/elasticsearch.vue
+++ themes/vakver/js/components/elasticsearch.vue
@@ -80,7 +80,8 @@
 		<div class="col-xs-6 col-lg-3" v-for="travel in row">
 			<div class="vacation-item">
 				<a href="/node/{{ travel._source.nid }}">
-					<div class="placeholder-img" v-if="travel._source.field_image_medium" v-lazy:background-image="travel._source.field_image_medium[0].url">
+					<div class="placeholder-img" v-if="travel._source.field_image_medium" 
+					v-lazy:background-image="travel._source.field_image_medium[0].url | rewritepath">
 
 						<!-- if more than 2 weeks old - 1209600 seconds -->
 						<div class="new-item" v-if="(Math.round((new Date()).getTime() / 1000) - travel._source.created) < 1209600">NIEUW</div>
@@ -106,7 +107,8 @@
 			<div class="vacation-item">
 				<a href="/node/{{ travel._source.nid }}">
 					<div class="col-md-3">
-						<div class="placeholder-img" v-if="travel._source.field_image_medium" v-lazy:background-image="travel._source.field_image_medium[0].url">
+						<div class="placeholder-img" v-if="travel._source.field_image_medium" 
+						v-lazy:background-image="travel._source.field_image_medium[0].url | rewritepath">
 
 							<!-- if more than 2 weeks old - 1209600 seconds -->
 							<div class="new-item" v-if="(Math.round((new Date()).getTime() / 1000) - travel._source.created) < 1209600">NIEUW</div>
